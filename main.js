9c04784249b6bc4ce42cf9da568fa0da3cf60521
Ibrahim Senzia <ibrahimsenzia@gmail.com>
2024-04-06 15:55:07 +0300

    liker completed

--- main.js
+++ main.js
@@ -4,7 +4,30 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+//Listeners to send back the request given
+document.addEventListener('DOMContentLoaded', () => {
+  const emptyHearts = document.querySelectorAll('.like-glyph');
+  const errorModal = document.getElementById('modal');
 
+  //adding a .hidden to remove the ERROR! heading visible after clicking the like icon
+  emptyHearts.forEach(emptyHeart => {
+    emptyHeart.addEventListener('click', () => {
+      mimicServerCall()
+        .then(() => {
+          emptyHeart.innerText = FULL_HEART;
+          emptyHeart.classList.add('activated-heart');
+          errorModal.classList.add('hidden');
+        })
+        .catch(() => {
+          errorModal.querySelector('#modal-message').innerText = 'Server Error';
+          errorModal.classList.remove('hidden');
+          setTimeout(() => {
+            errorModal.classList.add('hidden');
+          }, 3000);
+        });
+    });
+  });
+});
 
 
 //------------------------------------------------------------------------------
